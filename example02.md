9b5be7d65069a864d9e31bcfd4da27ef623a8e26
oaksoe <ozaw@oswego.edu>
2026-01-10 20:08:08 -0500

    learnt about git diff and git diff --cached

--- example02.md
+++ example02.md
@@ -1,1 +1,2 @@
 This is to test the pulling for git
+
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ def main():
     print("hello")
     print("Enter the age number")
 
+    age = input("Enter the age number")
+
 
 if __name__ == "__main__":
     main()
